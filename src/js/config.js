4fc2fb71780ef5e3107bc081c23118bde69c492a
BarberOfCoding <mihmegg@gmail.com>
2022-06-26 14:25:16 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '741e1b1c-0136-4825-886c-d70c58dd6fb5';
+export const MODAL_CLOSE_SEC = 2.5;
